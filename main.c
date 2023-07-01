cac86185e582122ddb7a4becce3684c62b162576
adam <adam@.gmail.com>
2023-07-01 03:45:53 -0700

    dld

--- main.c
+++ main.c
@@ -7,13 +7,13 @@
   * Return: return 0 or 1
   */
 
-int main(void)
+int main(__attribute__((unused))int argc, char *argv[])
 {
-	char *buff[1024];
+	char buff[1024];
 	FILE *file;
 	long long int n;
 
-	fopen(argv[0], "r");
+	file = fopen(argv[1], "r");
 	while (fgets(buff, 1024, file))
 	{
 		n = convert_number(buff);
--- main.h
+++ main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 long long int convert_number(char *buff);
 
